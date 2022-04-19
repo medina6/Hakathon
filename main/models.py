1aad85d4050eed2bc0e539942dd147a45967ff3c
Ubuntu <ubuntu@ip-172-31-95-5.ec2.internal>
2022-04-19 05:00:19 +0000

    some changes

--- main/models.py
+++ main/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='posts', default='default.jpg')
+    image = models.ImageField(upload_to='posts', default='default.jpg', blank=True, null=True)
 
     def __str__(self):
         return self.title
